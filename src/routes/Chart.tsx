74b0065f27c568cd2912c1baf1ae2629625f6d15
lallorona <chachaami66@gmail.com>
2022-05-10 11:16:59 +0900

    nested

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,5 @@
+function Chart() {
+  return <h1>Charffft</h1>;
+}
+
+export default Chart;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Params, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Outlet, Link } from "react-router-dom";
+import { useMatch } from "react-router-dom";
 
 interface RouteParams extends Params {
   coinId: string;
@@ -28,6 +30,49 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 interface RouteState {
   state: {
     name: string;
@@ -96,6 +141,10 @@ function Coin() {
 
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<IPriceData>();
+
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+  console.log(priceMatch);
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -110,17 +159,56 @@ function Coin() {
 
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || ":loading"}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
       {loading ? (
-        <Loader> ${priceInfo?.quotes.USD.ath_price}</Loader>
+        <Loader>Loading...</Loader>
       ) : (
-        priceInfo?.id
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
+          <Outlet />
+        </>
       )}
     </Container>
   );
